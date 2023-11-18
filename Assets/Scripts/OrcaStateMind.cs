67aa3ab8115ccad640cb44f92ca230ed8ff7f750
Jpporven <jpporven@gmail.com>
2023-11-18 17:49:06 -0500

    1.02 Update
    Added Orca whale encounter and a tutorial at the start of the game.

--- Assets/Scripts/OrcaStateMind.cs
+++ Assets/Scripts/OrcaStateMind.cs
@@ -11,38 +11,27 @@ public class OrcaStateMind : MonoBehaviour
     float distance0, distance1, distance2;
     int scorevalue = 0;
 
-    public bool orcaAwake;
-
-    private Coroutine coroutine;
-
     public platformMind platform;
 
     // Update is called once per frame
     void Update()
     {
-        if (1000 > scorevalue)
+        scorevalue = scoreMind.Score;
+
+        if (1000 < scorevalue)
         {
             OrcaGameobject.SetActive(true);
 
-            scorevalue = scoreMind.Score / 500;
-
-            orcaAwake = true;
-
-            coroutine = StartCoroutine(OrcaAwake(orcaAwake));
-            
+            OrcaAwake();
         }
-
-        if (coroutine != null) StopCoroutine(coroutine);
 
         distance0 = Vector3.Distance(OrcaPos[0].position, TargetPos[0].position);
         distance1 = Vector3.Distance(OrcaPos[1].position, TargetPos[1].position);
         distance2 = Vector3.Distance(OrcaPos[2].position, TargetPos[2].position);
     }
 
-    private IEnumerator OrcaAwake(bool awake)
+    private void OrcaAwake()
     {
-        yield return new WaitForSeconds(3);
-
         OrcaStates[0].SetActive(true);
 
         if (distance0 < 0.5f)
@@ -68,9 +57,6 @@ public class OrcaStateMind : MonoBehaviour
             distance2 = 100;
 
         }
-
-        awake = false;
-
     }
 
 }
